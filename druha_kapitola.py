6976a77a06a11b7d2080eda95949d48fb2c1245e
Julie Severova <145211284+Julie-is-forever@users.noreply.github.com>
2023-09-26 20:22:33 +0200

    Update and rename pzt.py to druha_kapitola.py

--- druha_kapitola.py
+++ druha_kapitola.py
@@ -120,3 +120,114 @@ x = 6        # initialize x
 print(x)
 x = x + 1    # update x
 print(x)
+
+
+
+
+#cviceni
+
+#1
+print(5 ** 2)
+print(9 * 5)
+print(15 / 12)
+print(12 / 15)
+print(15 // 12)
+print(12 // 15)
+print(5 % 2)
+print(9 % 5)
+print(15 % 12)
+print(12 % 15)
+print(6 % 6)
+print(0 % 7)
+
+#2
+print (2 + (3 - 1) * 10 / 5 * (2 + 3))
+
+#3
+str_hours = input("Please enter the time in hours")
+total_hours = int(str_hours)
+str_time = input("Please enter the time difference in hours")
+total_time = int(str_time)
+hours = total_hours + total_time
+time = hours // 24
+clock =  hours % 24
+print ("Hodiny budou ukazovat", clock)
+
+
+#4
+str_day = input("Please enter the day number")
+total_day = int(str_day)
+str_time = input("Please enter the lenght of your stay")
+total_time = int(str_time)
+stay = total_day + total_time
+time = stay // 7
+home =  stay % 7
+print ("Vratite se ", home, "den v tydnu")
+
+
+#5
+a = "All"
+b = "work"
+c = "and"
+d = "no"
+e = "play"
+f = "makes"
+g = "Jack"
+h = "a"
+ch = "dull"
+i = "boy"
+print (a,b,c,d,e,f,g,h,ch,i)
+
+#6
+print (6 * (1 - 2))
+
+#7
+
+str_t = input("Please enter the number of years")
+total_t = int(str_t)
+P= 10000
+n = 12
+r = 0.08
+compound =  P* (1+r/n) ** (n * total_t) 
+print ("The compound will be ", compound)
+
+#8
+
+str_r = input("Please enter the radius")
+total_r = int(str_r)
+pi= 3.14
+A =  pi* total_r**2
+print ("The areas of your circle is ", A)
+
+#9
+str_w = input("Please enter the width of your rectangle")
+total_w = int(str_w)
+str_h = input("Please enter the height of your rectangle")
+total_h = int(str_h)
+A =  total_h* total_w
+print ("The area of your rectangle is ", A)
+
+#10
+
+str_g = input("Please enter the number of gallons used")
+total_g = int(str_g)
+str_m = input("Please enter the number of miles travelled")
+total_m = int(str_m)
+MPG =  total_m / total_g
+print ("The MPG of your car is ", MPG, "miles per gallon")
+
+
+#11
+
+str_c = input("Please enter the temperature in celsius")
+total_c = int(str_c)
+f =  total_c * 9 / 5 + 32
+print ("The temperature in fahrenheit is ", f)
+
+
+#12
+
+str_f = input("Please enter the temperature in fahrenheit")
+total_f = int(str_f)
+c =  (total_f - 32)* 5 / 9
+print ("The temperature in celsius is ", c)
