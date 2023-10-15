1ec8649a5baf156a848666c2bb6e4f686b85c8f3
Julie Severova <145211284+Julie-is-forever@users.noreply.github.com>
2023-10-15 16:40:08 +0000

    o

--- X.py
+++ X.py
@@ -1,16 +1,18 @@
 import requests
 from bs4 import BeautifulSoup
+from tqdm import tqdm
 artworks=[]
 base_url = "https://collections.louvre.fr/en/recherche?typology%5B0%5D=1&page={}"
 total_pages = 534
-for page_number in range(1, total_pages + 1):
+for page_number in (pbar := tqdm(range(1,total_pages + 1))):
     URL = base_url.format(page_number)
     r = requests.get(URL)
     soup = BeautifulSoup(r.text, "html.parser")   
     painting_ = soup.find_all("h3", class_="card__title")
     for painting in painting_:
         p=(painting.text.strip())
-        print(p)
+        
+        pbar.write(p)
         artworks.append(p)
 
 
