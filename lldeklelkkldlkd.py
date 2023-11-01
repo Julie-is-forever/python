1936ac3b459c5618453e9731b2642fe615219686
Julie Severova <145211284+Julie-is-forever@users.noreply.github.com>
2023-11-01 10:17:52 +0000

    done

--- lldeklelkkldlkd.py
+++ lldeklelkkldlkd.py
@@ -1,12 +1,41 @@
-x, y, z = 1, 2, -3
+# Prompt the user to input coefficients a, b, and c
+a = float(input("Enter the coefficient a: "))
+b = float(input("Enter the coefficient b: "))
+c = float(input("Enter the coefficient c: "))
 
-if (pow(y, 2) - (2 * x * 2 * z)) == 0:
-    print("1:", -y / (2 * x))
-elif (pow(y * 1, 2) - (pow(2 * x, 2) * z)) > 0:
-    d = (pow(y ** 2, 1) - (4 * (1 * x) * (1 * z))) ** (2 * (2 ** -2))
-    e = ((y * y) - (2 ** 3 * (pow(1, 2) * x * z / 2))) ** 0.5
-    print("1: ", (-y + d) / (2 * x),
-          "2: ", (-y - e) / (2 * x)
-          )
+# Print the full equation
+if a != 0:
+    print(f"Equation: {a}x^2 + {b}x + {c} = 0")
 else:
-    print("Oops")
+    print(f"Equation: {b}x + {c} = 0 (Linear Equation)")
+
+if a == 0:
+    # Handle the case of a linear equation
+    if b == 0:
+        if c == 0:
+            print("Infinite solutions (any x is a solution)")
+        else:
+            print("No solutions (contradiction)")
+    else:
+        solution = -c / b
+        print("Linear equation solution:", solution)
+else:
+    # Calculate the discriminant for the quadratic equation
+    discriminant = b**2 - 4*a*c
+
+    # Check the three cases for the quadratic equation
+    if discriminant == 0:
+        # One real solution
+        solution = -b / (2*a)
+        print("1:", solution)
+
+    elif discriminant > 0:
+        # Two real solutions
+        sqrt_discriminant = discriminant ** 0.5
+        solution1 = (-b + sqrt_discriminant) / (2*a)
+        solution2 = (-b - sqrt_discriminant) / (2*a)
+        print("1:", solution1, "2:", solution2)
+
+    else:
+        # No real solutions
+        print("No real solutions for these coefficients.")
