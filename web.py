64fbf02d5c21ba019c20acc45b3add7081315aec
myke <MikulasZelinka@users.noreply.github.com>
2024-03-22 19:29:33 +0000

    web: kolikrat se komu libil film

--- web.py
+++ web.py
@@ -1,9 +1,27 @@
 
+# from collections import defaultdict
 from flask import Flask, render_template, redirect, url_for, request
 from tabulate import tabulate
 app = Flask(__name__)
 
-movies = []
+# kolikrat se film nekomu libil
+# movies = {}
+
+# defaultdict
+# movies = defaultdict(int)
+
+
+movies = {}
+movies = dict()
+
+movies = {
+   "Harry Potter": 3,
+   "Matrix": 1,
+}
+
+movies["Harry Potter"] = 42
+# with open("soubor.json") as f:
+#    movies = f.read()
 
 @app.route('/')
 def home():
@@ -12,11 +30,17 @@ def home():
    
 @app.route('/success/<name>')
 def success(name):
-   movies.append(name)
-   print(tabulate(movies))
+   # movies.append(name)
+
+
+   if name not in movies:
+      movies[name] = 0
+   movies[name] = movies[name] + 1
+
+   # print(tabulate(movies))
    return f"{name} is a great movie. Thank you for your answer, the current answers are {movies} \n\n"
 
-@app.route('/data',methods = ['POST'])
+@app.route('/data', methods = ['POST'])
 def data():
     user = request.form['fmovie']
     return redirect(url_for('success',name = user))
